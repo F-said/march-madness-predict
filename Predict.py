3ed6ee21aefa751e297c848c25bd1287ee707cc1
F-said <f.saidmuratov@yahoo.com>
2018-03-03 23:44:15 -0500

    Parameter and feature selection

--- Predict.py
+++ Predict.py
@@ -1,17 +1,26 @@
 from sklearn.dummy import DummyClassifier
-from sklearn.linear_model import SGDClassifier, LogisticRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.svm import LinearSVC, SVC
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, RandomizedSearchCV, GridSearchCV
 from sklearn.feature_selection import SelectFromModel
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-from sklearn.model_selection import RandomizedSearchCV
 from sklearn import preprocessing
+
+
+def feature_importance(X, y, classifier):
+    classifier.fit(X, y)
+
+    # Assess feature importance
+    importances = forest.feature_importances_
+    indices = np.argsort(importances)[::-1]
+    for f in range(X_train.shape[1]):
+        print("%2d) %-*s %f" % (f + 1, 30, list(X)[indices[f]], importances[indices[f]]))
+
+
 
 X_train = pd.read_csv("X_train_seedordinal.csv").drop(labels="Season", axis=1).drop(labels="Team1", axis=1).\
     drop(labels="Team2", axis=1)
@@ -55,79 +64,50 @@ dum_crossval = cross_val_score(dum, X_train, y_train, cv=fold_size)
 
 '''Learners'''
 ### Log Regression ###
-#logreg = LogisticRegression()
-#log_param_grid = [{'C': [0.01, 0.1, 1], 'class_weight': ['balanced', None], 'penalty': ['l1', 'l2']}]
-#clf = GridSearchCV(logreg, log_param_grid)
-#clf.fit(X_train, y_train)
+X_train_scale = preprocessing.minmax_scale(X_train)
+clf = LogisticRegression(C=0.001)
+clf.fit(X_train_scale, y_train)
 
 ### kNN classifier ###
-#nearest = KNeighborsClassifier()
-#knn_param_grid = [{'n_neighbors': [2, 4, 6, 8], 'weights': ['uniform', 'distance'],
-#                   'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']}]
-#knn = GridSearchCV(nearest, knn_param_grid)
-#knn.fit(X_train, y_train)
-
-### Linear SVC ###
-#linsvc = LinearSVC()
-#lsvc_param_grid = [{'C': [0.01, 0.1, 1], 'class_weight': ['balanced', None]}]
-#lsvc = GridSearchCV(linsvc, lsvc_param_grid)
-#lsvc.fit(X_train, y_train)
-
-### SVC ###
-#Supportvc = SVC(probability=True)
-#svc_param_grid = [{'C': [0.01, 0.1, 1], 'class_weight': ['balanced', None], 'kernel': ['rbf', 'linear', 'poly', 'sigmoid'
-#                                                                                           'precomputed']}]
-#svc = GridSearchCV(Supportvc, svc_param_grid)
-#svc.fit(X_train, y_train)
+knn = KNeighborsClassifier(algorithm='auto', n_neighbors=10)
+knn.fit(X_train, y_train)
 
 ### Random Forest ###
-#randforest = RandomForestClassifier()
-#forest_param_grid = {'criterion': ['entropy', 'gini'], 'n_estimators': [50, 100, 150, 200, 500],
-#                     'max_depth': [3, 4, 5, 6, 7], 'max_features': ['auto', 'sqrt', 'log2', None]}
-#forest = RandomizedSearchCV(randforest, forest_param_grid, cv=100)
-#forest.fit(X_train, y_train)
-#print(forest.get_params())
+forest = RandomForestClassifier(criterion="entropy", n_estimators=50, n_jobs=-1, max_depth=5, oob_score=True,
+                                max_features="auto", random_state=42)
+print("Random Forest Feature Importance")
+feature_importance(X_train, y_train, forest)
 
-### Decision Tree ###
-#dtree = DecisionTreeClassifier()
-#tree_param_grid = [{'criterion': ['entropy', 'gini'], 'max_depth': [3, 4, 5, 6, 7,
-#                   'max_features': ['auto', 'sqrt', 'log2', None]}]
-#tree = GridSearchCV(dtree, tree_param_grid)
-#tree.fit(X_train, y_train)
+t = float(input("input thresh:"))
 
-### SGD ###
-#sgd = SGDClassifier(loss="log")
-#sgd.fit(X_train, y_train)
+selection = SelectFromModel(estimator=forest, threshold=t)
+selection.fit(X_train, y_train)
+X_train_selected_forest = selection.transform(X_train)
+X_test_selected_forest = selection.transform(X_test)
+
+forest.fit(X_train_selected_forest, y_train)
+
 
 '''KFold Cross-Validator'''
-#learners = ("LogReg", "kNN", "L-SVC", "RandomF", "Tree", "SGD", "SVC")
-#y_pos = np.arange(len(learners))
-#performance = []
-#clf_crossval = cross_val_score(clf, X_train, y_train, cv=fold_size)
-#knn_crossval = cross_val_score(knn, X_train, y_train, cv=fold_size)
-#lsvc_crossval = cross_val_score(lsvc, X_train, y_train, cv=fold_size)
-#forest_crossval = cross_val_score(forest, X_train, y_train, cv=fold_size)
-#tree_crossval = cross_val_score(tree, X_train, y_train, cv=fold_size)
-#sgd_crossval = cross_val_score(sgd, X_train, y_train, cv=fold_size)
-#svc_crossval = cross_val_score(svc, X_train, y_train, cv=fold_size)
+learners = ("LogReg", "kNN", "RandomF")
+y_pos = np.arange(len(learners))
+performance = []
+clf_crossval = cross_val_score(clf, X_train, y_train, cv=fold_size)
+knn_crossval = cross_val_score(knn, X_train, y_train, cv=fold_size)
+forest_crossval = cross_val_score(forest, X_train_selected_forest, y_train, cv=fold_size)
 
-#performance.append(clf_crossval.mean())
-#performance.append(knn_crossval.mean())
-#performance.append(lsvc_crossval.mean())
-#performance.append(forest_crossval.mean())
-#performance.append(tree_crossval.mean())
-#performance.append(sgd_crossval.mean())
-#performance.append(svc_crossval.mean())
+performance.append(clf_crossval.mean())
+performance.append(knn_crossval.mean())
+performance.append(forest_crossval.mean())
 
-#for n, name in enumerate(learners):
-#    print(name, ":", performance[n])
+for n, name in enumerate(learners):
+    print(name, ":", performance[n])
 
 '''Submit predictions'''
-#probability = pd.DataFrame(forest.predict_proba(X_test))
+probability = pd.DataFrame(forest.predict_proba(X_test_selected_forest))
 
-#pred = pd.Series(probability[1])
-#sub_file.insert(1, "Pred", pred)
+pred = pd.Series(probability[1])
+sub_file.insert(1, "Pred", pred)
 
-#sub_file.to_csv(path_or_buf="submission_seedordinal.csv", index=False)
+sub_file.to_csv(path_or_buf="submission_seedordinal.csv", index=False)
 
-'''EVERYTHING WILL BE COMMENTED UNTIL I MAKE A PLAN'''
