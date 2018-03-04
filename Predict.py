39c97ec9e37c5f6d5637ba41490f1ee18c958928
F-said <f.saidmuratov@yahoo.com>
2018-03-04 01:19:39 -0500

    Added ensemble methods for logistic regression and knn

--- Predict.py
+++ Predict.py
@@ -1,6 +1,6 @@
 from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
@@ -19,7 +19,6 @@ def feature_importance(X, y, classifier):
     indices = np.argsort(importances)[::-1]
     for f in range(X_train.shape[1]):
         print("%2d) %-*s %f" % (f + 1, 30, list(X)[indices[f]], importances[indices[f]]))
-
 
 
 X_train = pd.read_csv("X_train_seedordinal.csv").drop(labels="Season", axis=1).drop(labels="Team1", axis=1).\
@@ -64,17 +63,20 @@ dum_crossval = cross_val_score(dum, X_train, y_train, cv=fold_size)
 
 '''Learners'''
 ### Log Regression ###
-X_train_scale = preprocessing.minmax_scale(X_train)
-clf = LogisticRegression(C=0.001)
-clf.fit(X_train_scale, y_train)
+clf = LogisticRegression(C=0.0001)
+
+bag_log = BaggingClassifier(base_estimator=clf, n_estimators=50, n_jobs=-1, oob_score=True, random_state=42, verbose=2)
+bag_log.fit(X_train, y_train)
 
 ### kNN classifier ###
-knn = KNeighborsClassifier(algorithm='auto', n_neighbors=10)
-knn.fit(X_train, y_train)
+knn = KNeighborsClassifier(algorithm='kd_tree', n_neighbors=50)
+
+bag_knn = BaggingClassifier(base_estimator=knn, n_estimators=50, n_jobs=-1, oob_score=True, random_state=42, verbose=2)
+bag_knn.fit(X_train, y_train)
 
 ### Random Forest ###
-forest = RandomForestClassifier(criterion="entropy", n_estimators=50, n_jobs=-1, max_depth=5, oob_score=True,
-                                max_features="auto", random_state=42)
+forest = RandomForestClassifier(criterion="entropy", n_estimators=150, n_jobs=-1, max_depth=5, oob_score=True,
+                                max_features="auto", random_state=42, verbose=2)
 print("Random Forest Feature Importance")
 feature_importance(X_train, y_train, forest)
 
@@ -92,19 +94,26 @@ forest.fit(X_train_selected_forest, y_train)
 learners = ("LogReg", "kNN", "RandomF")
 y_pos = np.arange(len(learners))
 performance = []
-clf_crossval = cross_val_score(clf, X_train, y_train, cv=fold_size)
-knn_crossval = cross_val_score(knn, X_train, y_train, cv=fold_size)
+performance_train = []
+
+bag_log_crossval = cross_val_score(bag_log, X_train, y_train, cv=fold_size)
+bag_knn_crossval = cross_val_score(bag_knn, X_train, y_train, cv=fold_size)
 forest_crossval = cross_val_score(forest, X_train_selected_forest, y_train, cv=fold_size)
 
-performance.append(clf_crossval.mean())
-performance.append(knn_crossval.mean())
+performance.append(bag_log_crossval.mean())
+performance.append(bag_knn_crossval.mean())
 performance.append(forest_crossval.mean())
 
+performance_train.append(bag_log.score(X_train, y_train))
+performance_train.append(bag_knn.score(X_train, y_train))
+performance_train.append(forest.score(X_train_selected_forest, y_train))
+
 for n, name in enumerate(learners):
-    print(name, ":", performance[n])
+    print(name, "train:", performance_train[n])
+    print(name, "cv:", performance[n], "\n")
 
 '''Submit predictions'''
-probability = pd.DataFrame(forest.predict_proba(X_test_selected_forest))
+probability = pd.DataFrame(bag_log.predict_proba(X_test))
 
 pred = pd.Series(probability[1])
 sub_file.insert(1, "Pred", pred)
