2f49c9630d49f0e1e93f8b3e25604643f5d0c9ae
F-said <f.saidmuratov@yahoo.com>
2018-03-07 21:34:20 -0500

    Lowest log loss with Gradient Boosting Classifier

--- Predict.py
+++ Predict.py
@@ -1,5 +1,5 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 import pandas as pd
@@ -8,17 +8,23 @@ from sklearn.model_selection import cross_val_score, RandomizedSearchCV, GridSea
 from sklearn.feature_selection import SelectFromModel
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-from sklearn import preprocessing
+
 
 '''FOR TUNING HYPERPARAMETERS AND SUBMITTING PREDICTIONS'''
 '''NOT FINAL SUBMISSION SELECTION SUNDAY'''
 
-X_train = pd.read_csv("X_train_seedordinal_selected.csv")
+X_train_selected = pd.read_csv("X_train_seedordinal_selected.csv")
+X_train_pca = pd.read_csv("X_train_seedordinal_pca.csv")
+X_train = pd.read_csv("X_train_seedordinal.csv").drop(labels="Season", axis=1).drop(labels="Team1", axis=1).\
+    drop(labels="Team2", axis=1)
 
 y_train = pd.read_csv("y_train_seedordinal.csv")
 y_train = y_train["Result"]
 
-X_test = pd.read_csv("X_test_seedordinal_selected.csv")
+X_test_selected = pd.read_csv("X_test_seedordinal_selected.csv")
+X_test_pca = pd.read_csv("X_test_seedordinal_pca.csv")
+X_test = pd.read_csv("X_test_seedordinal.csv").drop(labels="Season", axis=1).drop(labels="Team1", axis=1).\
+    drop(labels="Team2", axis=1)
 
 path = "/Users/farukhsaidmuratov/PycharmProjects/MarchMadness/"
 sub_file = pd.read_csv(path + "SampleSubmissionStage1.csv").drop(labels="Pred", axis=1)
@@ -50,19 +56,28 @@ svc.fit(X_train, y_train)
 svc_crossval = cross_val_score(svc, X_train, y_train, cv=fold_size)
 
 ### Random Forest ###
-forest = RandomForestClassifier(criterion='entropy', max_depth=5, max_features='auto', n_estimators=150, oob_score=True)
+forest = RandomForestClassifier(n_jobs=-1, n_estimators=50, criterion='gini', max_features='sqrt', max_depth=5,
+                                random_state=42, oob_score=True)
 forest.fit(X_train, y_train)
 
 forest_crossval = cross_val_score(forest, X_train, y_train, cv=fold_size)
+
+### Gradient Boosting ###
+gb = GradientBoostingClassifier(n_estimators=50, max_features='sqrt', max_depth=5, random_state=42, learning_rate=0.01)
+gb.fit(X_train, y_train)
+
+gb_crossval = cross_val_score(gb, X_train, y_train, cv=fold_size)
+
 
 '''KFold Cross Val Accuracy'''
 print("bag_log CV Score: ", bag_log_crossval.mean())
 print("knn CV Score: ", knn_crossval.mean())
 print("Svc CV Score: ", svc_crossval.mean())
 print("Forest CV Score: ", forest_crossval.mean())
+print("GB CV Score: ", gb_crossval.mean())
 
 '''Submit predictions'''
-probability = pd.DataFrame(forest.predict_proba(X_test))
+probability = pd.DataFrame(gb.predict_proba(X_test))
 
 pred = pd.Series(probability[1])
 sub_file.insert(1, "Pred", pred)
