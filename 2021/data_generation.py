4f11cefbc03cd454ab8648d07720efb64886e95e
unknown <saidmf@rpi.edu>
2021-03-09 18:50:45 -0500

    Created code to generate files within 2021 repo

---
+++ 2021/data_generation.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import ordinal_pkgr.feature_extraction as feat 
+
+year = "2021"
+'''Create Global Datasets and variables'''
+# Import data regarding team NCAA stats
+NCAADetailed = pd.read_csv(year + "\\unform_data\MNCAATourneyDetailedResults.csv").drop(labels="DayNum", axis=1)
+
+# Import team seeds
+Seeds = pd.read_csv(year + "\\unform_data\MNCAATourneySeeds.csv")
+# Drop rows where season is before 2003, since Detailed Results start at 2003.
+Seeds = Seeds[Seeds["Season"] >= 2003]
+# We only care about seed ranking, so take off region
+Seeds["Seed"] = Seeds["Seed"].apply(lambda s: int(s[1:3]))
+
+# Create ordinals_new 
+createOrdinals(year)
+Ordinals_new = pd.read_csv(year+"\\form_data\Ordinals_new.csv")
+
+'''Creation Training & Testing Data'''
+# Test
+target = initTestData(year) 
+# Train
+initTrainData(year, target)
